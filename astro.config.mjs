f22ad2e888714f6da5736af7e3b41583458c1993
kodamamnma <k4923590@kadai.jp>
2025-07-20 12:29:00 +0900

    7/20

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 // Import the Vercel adapter
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
 
-// https://astro.build/config
+// https://astro.build/configs
 export default defineConfig({
   site: "https://hamada-ryo.vercel.app/",
   integrations: [tailwind()],
